4f197439f273234170069fb6846ea7b5bb1978b6
TaliaGoncalves <gtalia246@gmail.com>
2024-10-15 23:12:08 -0300

    refactor: redefinindo game

--- BatalhaDeMonstros/Game.cs
+++ BatalhaDeMonstros/Game.cs
@@ -1,19 +1,12 @@
-using System;
-using System.Collections.Generic;
-
 public class Game
 {
-    private Player player1;
-    private Player player2;
+    private Player? player1; // Declarado como anulável
+    private Player? player2; // Declarado como anulável
     private MonsterFactory _monsterFactory;
-    private Stack<Monster.Memento> _mementosPlayer1;
-    private Stack<Monster.Memento> _mementosPlayer2;
 
     public Game()
     {
         _monsterFactory = new MonsterFactory();
-        _mementosPlayer1 = new Stack<Monster.Memento>();
-        _mementosPlayer2 = new Stack<Monster.Memento>();
     }
 
     public void Start()
@@ -24,65 +17,118 @@ public class Game
 
     private void MainMenu()
     {
-        Console.WriteLine("1. Jogar contra IA");
+        Console.WriteLine("Escolha o modo de jogo:");
+        Console.WriteLine("1. Jogar contra a IA");
         Console.WriteLine("2. Jogar contra outro jogador");
-        string choice = Console.ReadLine();
+        string modeChoice = Console.ReadLine();
 
-        if (choice == "1")
+        if (modeChoice == "1")
         {
-            // Jogar contra IA, criando monstros aleatórios
-            player1 = new Player(_monsterFactory.CreateRandomMonster());
-            player2 = new Player(_monsterFactory.CreateRandomMonster()); // IA como Player 2
-            Battle(player1, player2);
+            StartGameAgainstAI();
         }
-        else if (choice == "2")
+        else if (modeChoice == "2")
         {
-            // Jogar contra outro jogador, criando monstros aleatórios
-            player1 = new Player(_monsterFactory.CreateRandomMonster());
-            player2 = new Player(_monsterFactory.CreateRandomMonster());
-            Battle(player1, player2);
+            StartGameAgainstPlayer();
         }
         else
         {
-            Console.WriteLine("Escolha inválida. Tente novamente.");
-            MainMenu(); // Repete o menu se a escolha for inválida
+            Console.WriteLine("Escolha inválida, tente novamente.");
+            MainMenu();
         }
+    }
+
+    private void StartGameAgainstAI()
+    {
+        Console.WriteLine("Escolha o monstro do jogador 1:");
+        Console.WriteLine("1. Robô");
+        Console.WriteLine("2. Elfo");
+        Console.WriteLine("3. Zombie");
+        string choice1 = Console.ReadLine();
+
+        player1 = CreatePlayer(choice1);
+        player2 = CreateAIPlayer(); // Cria o jogador 2 como IA
+
+        Battle(player1, player2);
+    }
+
+    private void StartGameAgainstPlayer()
+    {
+        Console.WriteLine("Escolha o monstro do jogador 1:");
+        Console.WriteLine("1. Robô");
+        Console.WriteLine("2. Elfo");
+        Console.WriteLine("3. Zombie");
+        string choice1 = Console.ReadLine();
+
+        player1 = CreatePlayer(choice1);
+
+        Console.WriteLine("Escolha o monstro do jogador 2:");
+        string choice2 = Console.ReadLine();
+        player2 = CreatePlayer(choice2);
+
+        Battle(player1, player2);
+    }
+
+    private Player CreatePlayer(string choice)
+    {
+        Monster monster = choice switch
+        {
+            "1" => new Robot(),
+            "2" => new Elfo(),
+            "3" => new Zombie(),
+            _ => throw new ArgumentException("Escolha inválida")
+        };
+        return new Player(monster);
+    }
+
+    private Player CreateAIPlayer()
+    {
+        Monster monster = _monsterFactory.CreateRandomMonster(); // Cria um monstro aleatório
+        Console.WriteLine($"IA escolheu: {monster.Name}");
+        return new Player(monster);
     }
 
     private void Battle(Player player1, Player player2)
     {
-        // Lógica para batalha entre os jogadores
         Console.WriteLine("Iniciando batalha entre " + player1.Monster.Name + " e " + player2.Monster.Name);
-        // Implemente a lógica de batalha aqui
+
+        while (player1.Monster.Health > 0 && player2.Monster.Health > 0)
+        {
+            player1.ChooseAction(player2); // Jogador 1 escolhe ação
+            
+            // IA se defende se o jogador humano atacou
+            player2.Monster.SetDefending(false); // Reseta a defesa após a ação do jogador
+            if (player2.Monster.Health <= 0)
+            {
+                Console.WriteLine($"{player2.Monster.Name} foi derrotado!");
+                break;
+            }
+
+            // IA ataca após a ação do jogador
+            AIChooseAction(player2, player1); // Jogador 2 (IA) escolhe ação
+            
+            if (player1.Monster.Health <= 0)
+            {
+                Console.WriteLine($"{player1.Monster.Name} foi derrotado!");
+                break;
+            }
+        }
     }
 
-    private void SaveMonsterState(Player player)
+    private void AIChooseAction(Player aiPlayer, Player target)
     {
-        var memento = player.Monster.SaveState();
-        if (player == player1)
+        Random rand = new Random();
+        int actionChoice = rand.Next(1, 3); // 1 ou 2 (atacar ou defender)
+
+        if (actionChoice == 1)
         {
-            _mementosPlayer1.Push(memento);
+            int damage = aiPlayer.Monster.NormalAttack();
+            target.Monster.TakeDamage(damage);
+            Console.WriteLine($"{aiPlayer.Monster.Name} ataca {target.Monster.Name} causando {damage} de dano!");
         }
         else
         {
-            _mementosPlayer2.Push(memento);
-        }
-        Console.WriteLine("Estado salvo!");
-    }
-
-    private void RestoreMonsterState(Player player)
-    {
-        if (player == player1 && _mementosPlayer1.Count > 0)
-        {
-            player.Monster.RestoreState(_mementosPlayer1.Pop());
-        }
-        else if (player == player2 && _mementosPlayer2.Count > 0)
-        {
-            player.Monster.RestoreState(_mementosPlayer2.Pop());
-        }
-        else
-        {
-            Console.WriteLine("Nenhum estado salvo para restaurar!");
+            aiPlayer.Monster.SetDefending(true);
+            Console.WriteLine($"{aiPlayer.Monster.Name} está se defendendo!");
         }
     }
 }
--- BatalhaDeMonstros/GameState.cs
+++ BatalhaDeMonstros/GameState.cs
@@ -1,8 +1,11 @@
-using System;
-
-[Serializable]
 public class GameState
 {
-    public Player? Player { get; set; }
-    public Monster? EnemyMonster { get; set; }
+    public Player Player { get; private set; }
+    public Monster EnemyMonster { get; private set; }
+
+    public GameState(Player player, Monster enemyMonster)
+    {
+        Player = player ?? throw new ArgumentNullException(nameof(player));
+        EnemyMonster = enemyMonster ?? throw new ArgumentNullException(nameof(enemyMonster));
+    }
 }
