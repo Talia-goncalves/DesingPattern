76a012f3b1be3bada44c6d7e9ddc9958f75a6aa1
TaliaGoncalves <gtalia246@gmail.com>
2024-10-15 21:20:41 -0300

    feat: deixando a escolha do monstro aleatoria

--- BatalhaDeMonstros/MonsterFactory.cs
+++ BatalhaDeMonstros/MonsterFactory.cs
@@ -1,5 +1,9 @@
+using System;
+
 public class MonsterFactory
 {
+    private static readonly string[] MonsterTypes = { "elfo", "zumbi", "robô" }; // Tipos de monstros
+
     public Monster CreateMonster(string type)
     {
         return type.ToLower() switch
@@ -9,5 +13,13 @@ public class MonsterFactory
             "robô" => new Robot(),
             _ => throw new ArgumentException("Tipo de monstro inválido")
         };
+    }
+
+    // Método para criar um monstro aleatório
+    public Monster CreateRandomMonster()
+    {
+        var random = new Random();
+        string randomType = MonsterTypes[random.Next(MonsterTypes.Length)];
+        return CreateMonster(randomType);
     }
 }
