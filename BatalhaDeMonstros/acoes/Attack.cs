602b3d4a2406d25103361ce71c713583abd95e77
TaliaGoncalves <gtalia246@gmail.com>
2024-10-15 21:38:57 -0300

    fix: alterando o attack

--- BatalhaDeMonstros/acoes/Attack.cs
+++ BatalhaDeMonstros/acoes/Attack.cs
@@ -2,8 +2,6 @@ public class AttackAction : IActionStrategy
 {
     public void Execute(Monster attacker, Monster target)
     {
-        int damage = attacker.Attack();
-        target.TakeDamage(damage);
-        Console.WriteLine($"{attacker.Name} atacou {target.Name} e causou {damage} de dano!");
+        target.TakeDamage(attacker.Attack);
     }
 }
